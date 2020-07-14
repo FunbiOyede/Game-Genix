9ca39e5b3657a2597b8ec1bfe597814bb58d760a
Oyede Oluwafunbi <oyedetobi49@gmail.com>
2020-07-14 03:12:48 +0100

    chore(basic): Added basic funtioncality

---
+++ GameGenix/GameGenix/StartUp.xaml.cs
@@ -0,0 +1,28 @@
+﻿using GameGenix.views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GameGenix
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartUp : ContentPage
+    {
+        public StartUp()
+        {
+            InitializeComponent();
+            SimulateStartUp();
+        }
+
+        protected  async void SimulateStartUp()
+        {
+            await Task.Delay(3000);
+            await this.Navigation.PushAsync(new RootPage());
+        }
+    }
+}
